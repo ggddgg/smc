8d42f1f7ef7ce76c56ead425ad16ab71054a22d3
kim <yeen8443@gmail.com>
2020-07-14 09:19:47 +0900

    0714수정

---
+++ 0714/src/TestExam.java
@@ -0,0 +1,24 @@
+
+public class TestExam {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <=5; i++) {
+			
+			System.out.print(i+" ");
+			
+		}
+		
+		System.out.println(" ");
+		
+		for(int i = 0; i <=4; i++) {
+			
+			int a = 10;
+			int b = a-i;
+			System.out.print(b+" ");
+			
+		}
+
+	}
+
+}
